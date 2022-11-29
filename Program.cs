ca21344b81d1217e18f3403c36e25ab993b8fc64
Seaborn Mercer <surfermercer@gmail.com>
2022-11-29 16:11:48 -0600

    Phase Two finished

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace PlanYourHeist
 {
     class Program
@@ -15,8 +15,17 @@ namespace PlanYourHeist
             */
 
             //Prompt the user to enter a team member's name and save that name.   
+
+            List<Member> memberList = new List<Member>();
+
+            bool stillInputting = true;
+            while(stillInputting) {
             Console.WriteLine("Enter team member's name:");
             string memberName = Console.ReadLine();
+            if(memberName == "") {
+                stillInputting = false;
+                break;
+            }
 
             Console.WriteLine("Enter team member's skill level:");
             string answerTwo = Console.ReadLine();
@@ -27,11 +36,17 @@ namespace PlanYourHeist
             double courageFactor = Double.Parse(answerThree);
 
             Member ryan = new Member(memberName, result, courageFactor);
+            memberList.Add(ryan);
 
-        
-            Console.WriteLine(ryan.Name);
-            Console.WriteLine(ryan.SkillLevel);
-            Console.WriteLine(ryan.CourageFactor);
+            }
+
+            Console.WriteLine("This team is comprised of " + memberList.Count + " people");
+
+            for(int i = 0; i < memberList.Count; i++) {
+                Console.WriteLine(memberList[i].Name);
+                Console.WriteLine("he/she has a skill level of " + memberList[i].SkillLevel);
+                Console.WriteLine("he/she has a courage factor of " + memberList[i].CourageFactor);
+            }
 
         }
     }
